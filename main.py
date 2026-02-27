b82dd00bbce4061034a651725bdd06e96aa38efc
Carlo <261462775+carloBehindLabs@users.noreply.github.com>
2026-02-27 13:34:10 +0000

    final solution without testing

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-first_template = "It was about (Number) (Measure of time) ago when I arrived at the hospital in a (Mode of Transportation). The hospital is a/an (Adjective) place, there are a lot of (Adjective2) (Noun) here. There are nurses here who have (Color) (Part of the Body ). If someone wants to come into my room I told them that they have to (Verb) first. I’ve decorated my room with (Number2) (Noun2). Today I talked to a doctor and they were wearing a (Noun3) on their ( Part of the Body 2). I heard that all doctors (Verb) (Noun4) every day for breakfast. The most ( Adjective3) thing about being in the hospital is the (Silly Word ) (Noun) ! "
-second_template = "This weekend I am going camping with ( Proper Noun (Person’s Name)). I packed my lantern, sleeping bag, and (Noun). I am so (Adjective (Feeling)) to (Verb) in a tent. I am (Adjective (Feeling) 2) we might see a(n) (Animal), I hear they’re kind of dangerous. While we’re camping, we are going to hike, fish, and (Verb2). I have heard that the (Color) lake is great for ( Verb (ending in ing) ). Then we will (Adverb (ending in ly)) hike through the forest for (Number) (Measure of Time). If I see a (Color) (Animal) while hiking, I am going to bring it home as a pet! At night we will tell (Number) (Silly Word) stories and roast (Noun2) around the campfire!! "
-third_template = "Dear (Proper Noun (Person’s Name) ), I am writing to you from a (Adjective) castle in an enchanted forest. I found myself here one day after going for a ride on a (Color) (Animal) in (Place). There are (Adjective2) (Magical Creature (Plural)) and (Adjective3) (Magical Creature (Plural)2) here! In the ( Room in a House) there is a pool full of (Noun). I fall asleep each night on a (Noun2) of (Noun(Plural)3) and dream of (Adjective4) ( Noun (Plural)4). It feels as though I have lived here for (Number) ( Measure of time). I hope one day you can visit, although the only way to get here now is (Verb (ending in ing)) on a (Adjective5) (Noun5)!!"
-
 def first_template() :
     number = int(input("Type Number: "))
     date = input("Type a Measure of Time: ")
@@ -16,11 +12,9 @@ def first_template() :
     noun2 = input("Type Noun 2: ")
     noun3 = input("Type Noun 3: ")
     body2 = input("Type Part of the Body 2: ")
-    # verb
     noun4 = input("Type Noun 4: ")
     adjective3 = input("Type Adjective 3: ")
     sillyWord = input("Type Silly Word: ")
-    # noun
 
     print("It was about " + number + " " + date + " ago when I arrived at the hospital in a " + transport + ".")
     print(" The hospital is a/an " + adjective + " place, there are a lot of " + adjective2 + " " + noun + " here.")
@@ -32,10 +26,59 @@ def first_template() :
     print(" The most " + adjective3 + " thing about being in the hospital is the " + sillyWord + " " + noun + "! ")
 
 def second_template() :
-    print("")
+    properNoun = int(input("Type Proper Noun (Person’s Name): "))
+    noun = input("Type Noun: ")
+    adjective = input("Type Adjective (Feeling): ")
+    verb = input("Type Verb: ")
+    animal = input("Type an Animal: ")
+    adjective2 = input("Type Adjective (Feeling) 2: ")
+    verb2 = input("Type Verb 2: ")
+    color = input("Type a Color: ")
+    verbing = input("Type a Verb (ending in ing): ")
+    adverb = input("Type Adverb (ending in ly): ")
+    number = input("Type Number : ")
+    date = input("Type Measure of Time: ")
+    noun2 = input("Type Noun 2: ")
+    sillyWord = input("Type Silly Word: ")
+
+    print("This weekend I am going camping with " + properNoun + ".")
+    print(" I packed my lantern, sleeping bag, and " + noun + ".")
+    print(" I am so " + adjective + " to " + verb + " in a tent.")
+    print(" I am " + adjective2 + " we might see a(n) " + animal + ", I hear they’re kind of dangerous.")
+    print(" While we’re camping, we are going to hike, fish, and " + verb2 + ".")
+    print(" I have heard that the " + color + " lake is great for " + verbing + ".")
+    print(" Then we will " + adverb + " hike through the forest for " + number + " " + date + ".")
+    print(" If I see a " + color + " " + animal + " while hiking, I am going to bring it home as a pet! At night we will tell " + number + " " + sillyWord + " stories and roast " + noun2 + " around the campfire!! ")
 
 def third_template() :
-    print("")
+    properNoun = int(input("Type Proper Noun (Person’s Name): "))
+    adjective = input("Type Adjective: ")
+    color = input("Type a Color: ")
+    animal = input("Type an Animal: ")
+    place = input("Type Place: ")
+    adjective2 = input("Type Adjective: ")
+    creature = input("Type Magical Creature (Plural): ")
+    adjective3 = input("Type Adjective 3: ")
+    creature2 = input("Type Magical Creature (Plural) 2: ")
+    room = input("Type Room in a House: ")
+    noun = input("Type Noun: ")
+    noun2 = input("Type Noun 2: ")
+    noun3 = input("Type Noun (Plural) 3: ")
+    adjective4 = input("Type Adjective 4: ")
+    noun4 = input("Type Noun (Plural) 4: ")
+    number = input("Type number: ")
+    date = input("Type Measure of Time: ")
+    verbing = input("Type a Verb (ending in ing): ")
+    adjective5 = input("Type Adjective 5: ")
+    noun5 = input("Type Noun 5: ")
+
+    print("Dear " + properNoun + ", I am writing to you from a " + adjective + " castle in an enchanted forest.")
+    print(" I found myself here one day after going for a ride on a " + color + " " + animal + " in " + place + ".")
+    print(" There are " + adjective2 + " " + creature + " and " + adjective3 + " " + creature2 + " here! In the " + room + " there is a pool full of " + noun + ".")
+    print(" I fall asleep each night on a " + noun2 + " of " + noun3 + " and dream of " + adjective4 + " " + noun4 + ".")
+    print(" It feels as though I have lived here for " + number + " " + date + ".")
+    print(" I hope one day you can visit, although the only way to get here now is " + verbing + " on a " + adjective5 + " " + noun5 + "!!")
+
 
 template = int(input("Which Template you going to pick (1, 2, 3): "))
 
